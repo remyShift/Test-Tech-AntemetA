831238c4fe718724e73f83ff9d38bda1b7f0d1ef
remyShift <remy.engassac@icloud.com>
2025-05-24 21:07:39 +0200

    remove useless middleware who handle error in my route

--- src/routes/transcription.ts
+++ src/routes/transcription.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import multer from 'multer';
 import { transcribeWav } from '../utils/transcribeWav';
 import { validateFile } from '../utils/fileValidation';
